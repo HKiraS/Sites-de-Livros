ac25e3027a1aaa5e775900f92ef2f71023c835f1
HKiraS <destroidormestre@gmail.com>
2024-09-18 13:16:55 -0500

    atualização no loadCapitulo.js - Renomeação de diversas funções. - Adicionado funções para navegação sem o reload da página - Melhora na estrutura do arquivo

--- js/modules/loadCapitulo.js
+++ js/modules/loadCapitulo.js
@@ -1,120 +1,152 @@
+import GetChapters from './chapterOptions.js';
+
+const chapterManager = new GetChapters();
+
 const urlParams = new URLSearchParams(window.location.search);
-let chapterId = +urlParams.get('id') || 1;
-const btnNext = document.querySelector('#btn-next');
-const btnPrevious = document.querySelector('#btn-previous');
+let currentChapterId = +urlParams.get('id') || 1;
+const nextButton = document.querySelector('#btn-next');
+const previousButton = document.querySelector('#btn-previous');
+const chapterMenu = document.querySelector('#chapters-menu');
 
+function readTime(text) {
+  const chapterTime = document.querySelector('.chapter-time span');
+  const wordCount = text.replace(/\n+/g, " ").split(' ').length;
+  const time = Math.round(wordCount / 200);
+  chapterTime.innerText = time + ' minutes';
+}
 
+function mapChaptersById(chaptersArray) {
+  return chaptersArray.reduce((acc, chapter) => {
+    acc[chapter.id] = chapter;
+    return acc;
+  }, {});
+}
 
-async function initLoadChapters() {
-  try {
-    const response = await fetch("./assets/JSON/referencia.JSON");
-    const chaptersData = await response.json();
-    const chaptersMap = mapChaptersById(chaptersData.capitulos);
+// Função que cria as opções de capítulos e sincroniza com o capítulo atual
+function createOptions(obj) {
+  chapterMenu.innerHTML = '';
 
-    displayChapterText(chaptersMap);
-    changeChapter(chaptersData);
-  } catch (error) {
-    console.error("Não foi possível carregar os capítulos. Erro: ", error);
-    alert('Não foi possível carregar os capítulos.');
-  }
-
-  function mapChaptersById(chaptersArray) {
-    return chaptersArray.reduce((acc, chapter) => {
-      acc[chapter.id] = chapter;
-      return acc;
-    }, {});
-  }
-
-
-  async function displayChapterText(chaptersMap) {
-    const chapterTextContainer = document.querySelector('.capitulo-texto');
-    const title = document.querySelector('#title-chapter');
-
-
-    const chapter = chaptersMap[chapterId];
-
-
-    if (chapter) {
-      try {
-        const chapterResponse = await fetch(chapter.caminho);
-        const chapterContent = await chapterResponse.text();
-        const img = document.querySelector('.chapter-image');
-        const imgBg = document.querySelector('.chapter-image-blur')
-        const data = document.querySelector('.publish-date');
-        const paragraphs = chapterContent.split('/n');
-        
-        readTime(chapterContent)
-
-        chapterTextContainer.innerHTML = '';
-
-
-        title.innerText = 'Capítulo ' + chapter.id + ' - ' + chapter.nome;
-        img.setAttribute('src', chapter.capa)
-        imgBg.style.backgroundImage = `url('${chapter.capa}')`
-        data.innerHTML = `Publicado em: <span class="vermelho">${chapter.data}</span>`
-
-        chapterTextContainer.classList.remove('flex')
-        chapterTextContainer.classList.remove('align-center')
-
-        paragraphs.forEach((paragraphText) => {
-          const paragraphElement = document.createElement('p');
-          paragraphElement.innerText = paragraphText;
-          chapterTextContainer.appendChild(paragraphElement);
-
-        });
-      } catch (error) {
-        console.error('Erro ao carregar o capítulo. Erro: ', error);
-        alert('Erro ao carregar o capítulo. Por favor, entre em contato.');
-      }
-    } else {
-      alert('Capítulo não encontrado. Redirecionando para a página inicial.');
-      window.location.href = './index.html';
+  obj.capitulos.forEach((chapter) => {
+    const option = document.createElement('option');
+    option.text = `Chapter ${chapter.id}`;
+    option.value = chapter.id;
+    if (chapter.id == currentChapterId) {
+      option.selected = true;
     }
-  }
+    chapterMenu.appendChild(option);
+  });
+}
 
-  async function changeChapter(info) {
-  
-    if (chapterId === info.capitulos.length) {
-      btnNext.setAttribute('disabled', '')
+// Função para atualizar a seleção do menu de capítulos
+function updateChapterSelection() {
+  const options = chapterMenu.options;
+  for (let i = 0; i < options.length; i++) {
+    if (+options[i].value === currentChapterId) {
+      options[i].selected = true;
+      break;
     }
-    
-    if (chapterId === 1) {
-      btnPrevious.setAttribute('disabled', '')
-    }
-  
-    btnPrevious.addEventListener('click', previousChapter)
-    btnNext.addEventListener('click', nextChapter)
-  
-    function nextChapter() {
-      chapterId++;
-      window.location.href = `./capitulo.html?id=${chapterId}`
-      if (chapterId === info.capitulos.length) {
-        btnNext.setAttribute('disabled', '')
-      }
-      btnPrevious.removeAttribute('disabled');
-    }
-  
-    function previousChapter() {
-      chapterId--;
-      window.location.href = `./capitulo.html?id=${chapterId}`
-      btnNext.removeAttribute('disabled');
-      if (chapterId === 1) {
-        btnPrevious.setAttribute('disabled', '');
-      }
-    }
-  }
-  
-
-  async function readTime(text) {
-    const chapterTime = document.querySelector('.chapter-time span');
-    const wordCount = text.replace(/\n+/g, " ").split(' ').length;
-    const time = Math.round(wordCount / 200 );
-    
-    chapterTime.innerText = time + ' minutos';
   }
 }
 
+async function updateChapter(chapterId, chaptersMap) {
+  // Atualizar a URL sem recarregar a página
+  history.pushState(null, '', `./chapter.html?id=${chapterId}`);
 
+  updateChapterSelection();
+  // Atualizar o conteúdo do capítulo dinamicamente
+  await displayChapterText(chaptersMap);
+  updateNavigationButtons(chaptersMap);
+}
 
+function updateNavigationButtons(chaptersMap) {
+  const totalChapters = Object.keys(chaptersMap).length;
+
+  nextButton.disabled = currentChapterId === totalChapters;
+  previousButton.disabled = currentChapterId === 1;
+}
+
+async function displayChapterText(chaptersMap) {
+  const chapterTextContainer = document.querySelector('.chapter-text');
+  const title = document.querySelector('#title-chapter');
+  const chapter = chaptersMap[currentChapterId];
+
+  if (chapter) {
+    try {
+      const chapterResponse = await fetch(chapter.path);
+      const chapterContent = await chapterResponse.text();
+      const img = document.querySelector('.chapter-image');
+      const imgBg = document.querySelector('.chapter-image-blur');
+      const publishDate = document.querySelector('.publish-date');
+      const paragraphs = chapterContent.split('/n');
+
+      readTime(chapterContent);
+
+      chapterTextContainer.innerHTML = '';
+      title.innerText = `Chapter ${chapter.id} -  ${chapter.name}`;
+      publishDate.innerHTML = `Published on: <span class="red">${chapter.date}</span>`;
+
+      img.setAttribute('src', chapter.cover);
+      imgBg.style.backgroundImage = `url('${chapter.cover}')`;
+
+      chapterTextContainer.classList.remove('flex');
+      chapterTextContainer.classList.remove('align-center');
+
+      paragraphs.forEach((paragraphText) => {
+        const paragraphElement = document.createElement('p');
+        paragraphElement.innerText = paragraphText;
+        chapterTextContainer.appendChild(paragraphElement);
+      });
+    } catch (error) {
+      console.error('Error loading chapter. Error: ', error);
+      alert('Error loading chapter. Please contact support.');
+    }
+  } else {
+    alert('Chapter not found. Redirecting to the home page.');
+    window.location.href = './index.html';
+  }
+}
+
+async function initLoadChapters() {
+  try {
+    const chaptersData = await chapterManager.getInfo("./assets/JSON/reference.JSON");
+    const chaptersMap = mapChaptersById(chaptersData.capitulos);
+
+    // Adiciona listener para quando o menu de seleção for alterado
+    chapterMenu.addEventListener('change', async (event) => {
+      currentChapterId = +event.target.value;
+      await updateChapter(currentChapterId, chaptersMap);
+    });
+    
+    previousButton.addEventListener('click', async () => {
+      currentChapterId--;
+      await updateChapter(currentChapterId, chaptersMap);
+    });
+
+    nextButton.addEventListener('click', async () => {
+      currentChapterId++;
+      await updateChapter(currentChapterId, chaptersMap);
+    });
+
+    // Carrega o capítulo atual na inicialização
+    displayChapterText(chaptersMap);
+    updateNavigationButtons(chaptersMap);
+    createOptions(chaptersData);
+  } catch (error) {
+    console.error("Unable to load chapters. Error: ", error);
+    alert('Unable to load chapters.');
+  }
+}
+
+// Evento para lidar com o botão de voltar/avançar do navegador
+window.addEventListener('popstate', async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  currentChapterId = +urlParams.get('id') || 1;
+
+  const chaptersData = await chapterManager.getInfo("./assets/JSON/reference.JSON");
+  const chaptersMap = mapChaptersById(chaptersData.capitulos);
+
+  // Atualiza o capítulo de acordo com a navegação do navegador
+  await updateChapter(currentChapterId, chaptersMap);
+});
 
 initLoadChapters();
